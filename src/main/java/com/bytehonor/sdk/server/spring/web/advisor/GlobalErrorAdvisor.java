9cd9e5c05eda646b72ece790f533f9687260cf2f
lijianqiang <newphper@gmail.com>
2019-03-04 11:26:30 +0800

    defaultErrorHandler

--- src/main/java/com/bytehonor/sdk/server/spring/web/advisor/GlobalErrorAdvisor.java
+++ src/main/java/com/bytehonor/sdk/server/spring/web/advisor/GlobalErrorAdvisor.java
@@ -13,29 +13,27 @@ import com.bytehonor.sdk.server.spring.web.error.stragety.ExceptionStragety;
 @ControllerAdvice
 public class GlobalErrorAdvisor {
 
-	private static final Logger LOG = LoggerFactory.getLogger(GlobalErrorAdvisor.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalErrorAdvisor.class);
 
-	@ResponseBody
-	@ExceptionHandler(value = Exception.class)
-	public final ExceptionEntity defaultErrorHandler(Exception ex) {
-		if (LOG.isWarnEnabled()) {
-			// 错误栈太多无用信息, 只打4行
-			StackTraceElement[] stackTrace = ex.getStackTrace();
-			StringBuilder sb = new StringBuilder("\n [*** Print ").append(ex.getClass().getSimpleName())
-					.append(" Stack]");
-			sb.append("\n [*** 0 ]:").append(ex.getMessage());
-			for (int i = 1; i < 5; i++) {
-				if (stackTrace[i] == null) {
-					continue;
-				}
-				sb.append("\n [*** ").append(i).append(" ]:").append(stackTrace[i].toString());
-			}
-			sb.append("\n [*** Print Exception Stack End] \n");
-			LOG.warn(sb.toString());
-		}
-		ExceptionStragety exceptionStragety = ExceptionStragetyFactory.build(ex);
-		ExceptionEntity error = exceptionStragety.process();
-		return error;
-	}
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public final ExceptionEntity defaultErrorHandler(Exception ex) {
+        if (LOG.isWarnEnabled()) {
+            // 错误栈太多无用信息, 只打5行
+            StackTraceElement[] stackTrace = ex.getStackTrace();
+            StringBuilder sb = new StringBuilder("\n *** ").append(ex.getClass().getSimpleName()).append(" : ")
+                    .append(ex.getMessage());
+            for (int i = 0; i < 5; i++) {
+                if (stackTrace[i] == null) {
+                    continue;
+                }
+                sb.append("\n *** ").append(i).append(" : ").append(stackTrace[i].toString());
+            }
+            LOG.warn(sb.toString());
+        }
+        ExceptionStragety exceptionStragety = ExceptionStragetyFactory.build(ex);
+        ExceptionEntity error = exceptionStragety.process();
+        return error;
+    }
 
 }
