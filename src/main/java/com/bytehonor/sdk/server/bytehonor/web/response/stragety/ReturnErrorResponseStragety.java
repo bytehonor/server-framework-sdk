5bf8423ee9e1b5af7598a67351f79b31872fc245
lijianqiang <343891954@qq.com>
2021-06-20 22:35:51 +0800

    5.3.4

--- src/main/java/com/bytehonor/sdk/server/bytehonor/web/response/stragety/ReturnErrorResponseStragety.java
+++ src/main/java/com/bytehonor/sdk/server/bytehonor/web/response/stragety/ReturnErrorResponseStragety.java
@@ -20,160 +20,142 @@ import com.bytehonor.sdk.server.bytehonor.web.response.helper.ResponseStragetyHe
 
 public final class ReturnErrorResponseStragety implements ResponseStragety {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ReturnErrorResponseStragety.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ReturnErrorResponseStragety.class);
 
-	private ExceptionHolder error;
+    private ExceptionHolder error;
 
-	private final ServerHttpResponse response;
+    private final ServerHttpResponse response;
 
-	private final SpringBootStandardProperties standardSpringBootProperties;
+    private final SpringBootStandardProperties standardSpringBootProperties;
 
-	private final boolean enableDebugRequest;
+    private final boolean enableForceStatus;
 
-	private final boolean enableForceStatus;
+    public ReturnErrorResponseStragety(ExceptionHolder error, ServerHttpResponse response,
+            SpringBootStandardProperties standardSpringBootProperties) {
+        this.response = response;
+        this.error = error;
+        this.standardSpringBootProperties = standardSpringBootProperties;
+        this.enableForceStatus = standardSpringBootProperties != null ? standardSpringBootProperties.isForceHttpStatus()
+                : false;
+    }
 
-	public ReturnErrorResponseStragety(ExceptionHolder error, ServerHttpResponse response,
-			SpringBootStandardProperties standardSpringBootProperties) {
-		this.response = response;
-		this.error = error;
-		this.standardSpringBootProperties = standardSpringBootProperties;
-		this.enableDebugRequest = standardSpringBootProperties != null
-				? standardSpringBootProperties.isRestfulDebugEnable()
-				: false;
-		this.enableForceStatus = standardSpringBootProperties != null ? standardSpringBootProperties.isForceHttpStatus()
-				: false;
-	}
+    @Override
+    public JsonResponse<?> process(ServerHttpRequest request) {
+        JsonResponse<Object> jsonResponse = new JsonResponse<Object>();
+        HttpStatus httpStatus = HttpStatus.valueOf(error.getStatus());
+        String showTip = "";
 
-	@Override
-	public JsonResponse<?> process(ServerHttpRequest request) {
-		JsonResponse<Object> jsonResponse = new JsonResponse<Object>();
-		HttpStatus httpStatus = HttpStatus.valueOf(error.getStatus());
-		String showTip = "";
+//		if (enableDebugRequest) {
+//			// 打印错误栈
+//			jsonResponse.setTrace(buildDebugErrorTrace(error));
+//		}
+        NormalErrorMessage normalMessage = buildNormalErrorMessage(error);
+        showTip = normalMessage.toString();
 
-		// if (enableDebugRequest) {
-		// DebugErrorMessage debugMessage = buildDebugErrorMessage(error, request);
-		// showTip = debugMessage.toString();
-		// jsonResponse.setTrace(buildDebugErrorTrace(error));
-		// } else {
-		// NormalErrorMessage normalMessage = buildNormalErrorMessage(error);
-		// showTip = normalMessage.toString();
-		// }
+        jsonResponse.setCode(error.getCode());
+        jsonResponse.setMessage(filterShowTip(showTip));
 
-		if (enableDebugRequest) {
-			// 打印错误栈
-			jsonResponse.setTrace(buildDebugErrorTrace(error));
-		}
-		NormalErrorMessage normalMessage = buildNormalErrorMessage(error);
-		showTip = normalMessage.toString();
+        // 重置Http status code 200
+        if (enableForceStatus || ResponseStragetyHelper.isForceHttpStatusOk(request.getHeaders())) {
+            httpStatus = HttpStatus.OK;
+        }
 
-		jsonResponse.setCode(error.getCode());
-		jsonResponse.setMessage(filterShowTip(showTip));
+        if (response != null) {
+            response.setStatusCode(httpStatus);
+        }
 
-		// 重置Http status code 200
-		if (enableForceStatus || ResponseStragetyHelper.isForceHttpStatusOk(request.getHeaders())) {
-			httpStatus = HttpStatus.OK;
-		}
+        if (LOG.isWarnEnabled()) {
+            LOG.warn("ErrorCode:{}, HttpStatus:{}", jsonResponse.getCode(), httpStatus.value());
+        }
 
-		if (response != null) {
-			response.setStatusCode(httpStatus);
-		}
+        return jsonResponse;
+    }
 
-		if (LOG.isWarnEnabled()) {
-			LOG.warn("ErrorCode:{}, HttpStatus:{}", jsonResponse.getCode(), httpStatus.value());
-		}
+    public List<String> buildDebugErrorTrace(ExceptionHolder error) {
+        List<String> trace = new ArrayList<String>();
+        Exception e = error.getException();
+        if (e == null) {
+            return trace;
+        }
+        StackTraceElement[] elements = e.getStackTrace();
+        for (int i = 0; i < 3; i++) {
+            if (elements[i] != null) {
+                trace.add(elements[i].toString());
+            }
+        }
+        return trace;
+    }
 
-		return jsonResponse;
-	}
+    @SuppressWarnings("unused")
+    private DebugErrorMessage buildDebugErrorMessage(ExceptionHolder error, ServerHttpRequest request) {
+        DebugErrorMessage message = new DebugErrorMessage();
+        Exception ex = error.getException();
+        message.setUri(formatUri(request.getURI()));
+        // message.setClientIp(request.getRemoteAddress().getAddress().getHostAddress());
+        if (ex != null) {
+            message.setError(ex.getClass().getSimpleName());
+            message.setDetail(ex.getMessage());
+            message.setStackTrace(formatTrace(ex)); // TODO
+        }
+        return message;
+    }
 
-	private List<String> buildDebugErrorTrace(ExceptionHolder error) {
-		List<String> trace = new ArrayList<String>();
-		Exception e = error.getException();
-		if (e == null) {
-			return trace;
-		}
-		StackTraceElement[] elements = e.getStackTrace();
-		for (int i = 0; i < 3; i++) {
-			if (elements[i] != null) {
-				trace.add(elements[i].toString());
-			}
-		}
-		return trace;
-	}
+    private NormalErrorMessage buildNormalErrorMessage(ExceptionHolder error) {
+        NormalErrorMessage message = new NormalErrorMessage();
+        Exception ex = error.getException();
+        if (ex != null) {
+            message.setContent(ex.getMessage() != null ? ex.getMessage() : ex.getClass().getSimpleName());
+        }
+        return message;
+    }
 
-	@SuppressWarnings("unused")
-	private DebugErrorMessage buildDebugErrorMessage(ExceptionHolder error, ServerHttpRequest request) {
-		DebugErrorMessage message = new DebugErrorMessage();
-		Exception ex = error.getException();
-		message.setUri(formatUri(request.getURI()));
-		// message.setClientIp(request.getRemoteAddress().getAddress().getHostAddress());
-		if (ex != null) {
-			message.setError(ex.getClass().getSimpleName());
-			message.setDetail(ex.getMessage());
-			message.setStackTrace(formatTrace(ex)); // TODO
-		}
-		return message;
-	}
+    private String filterShowTip(String showTip) {
+        if (showTip == null || showTip.length() < 1) {
+            return "unknown";
+        }
+        if (showTip.length() > 512) {
+            return showTip.substring(0, 512);
+        }
+        return showTip;
+    }
 
-	private NormalErrorMessage buildNormalErrorMessage(ExceptionHolder error) {
-		NormalErrorMessage message = new NormalErrorMessage();
-		Exception ex = error.getException();
-		if (ex != null) {
-			message.setContent(ex.getMessage() != null ? ex.getMessage() : ex.getClass().getSimpleName());
-		}
-		return message;
-	}
+    private String formatTrace(Exception ex) {
+        if (ex == null || ex.getStackTrace() == null || ex.getStackTrace().length < 1) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(ex.getStackTrace()[0].toString()); // first line
+        return sb.toString();
+    }
 
-	private String filterShowTip(String showTip) {
-		if (showTip == null || showTip.length() < 1) {
-			return "unknown";
-		}
-		if (showTip.length() > 512) {
-			return showTip.substring(0, 512);
-		}
-		return showTip;
-	}
+    private String formatUri(URI uri) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(uri.getPort()).append(uri.getPath());
+        if (uri.getQuery() != null) {
+            sb.append("?");
+            sb.append(uri.getQuery());
+        }
+        return sb.toString();
+    }
 
-	private String formatTrace(Exception ex) {
-		if (ex == null || ex.getStackTrace() == null || ex.getStackTrace().length < 1) {
-			return "";
-		}
-		StringBuilder sb = new StringBuilder();
-		sb.append(ex.getStackTrace()[0].toString()); // first line
-		return sb.toString();
-	}
+    public ExceptionHolder getError() {
+        return error;
+    }
 
-	private String formatUri(URI uri) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(uri.getPort()).append(uri.getPath());
-		if (uri.getQuery() != null) {
-			sb.append("?");
-			sb.append(uri.getQuery());
-		}
-		return sb.toString();
-	}
+    public void setError(ExceptionHolder error) {
+        this.error = error;
+    }
 
-	public ExceptionHolder getError() {
-		return error;
-	}
+    public ServerHttpResponse getResponse() {
+        return response;
+    }
 
-	public void setError(ExceptionHolder error) {
-		this.error = error;
-	}
+    public SpringBootStandardProperties getStandardSpringBootProperties() {
+        return standardSpringBootProperties;
+    }
 
-	public ServerHttpResponse getResponse() {
-		return response;
-	}
-
-	public boolean isEnableDebugRequest() {
-		return enableDebugRequest;
-	}
-
-	public SpringBootStandardProperties getStandardSpringBootProperties() {
-		return standardSpringBootProperties;
-	}
-
-	public boolean isEnableForceStatus() {
-		return enableForceStatus;
-	}
+    public boolean isEnableForceStatus() {
+        return enableForceStatus;
+    }
 
 }
