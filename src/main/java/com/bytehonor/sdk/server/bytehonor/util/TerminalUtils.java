bde572c4d6dc6bcac2f4c3dfb1d9d4883d6d5d26
lijianqiang <newphper@126.com>
2021-06-23 10:16:06 +0800

    5.3.10

--- src/main/java/com/bytehonor/sdk/server/bytehonor/util/TerminalUtils.java
+++ src/main/java/com/bytehonor/sdk/server/bytehonor/util/TerminalUtils.java
@@ -2,45 +2,13 @@ package com.bytehonor.sdk.server.bytehonor.util;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.bytehonor.sdk.define.bytehonor.constant.HeaderKey;
-import com.bytehonor.sdk.lang.bytehonor.string.StringObject;
-
-@Deprecated
 public class TerminalUtils {
 
     public static String getFromTerminal(HttpServletRequest request) {
-        String from = request.getParameter(HeaderKey.X_FROM_TERMINAL);
-        if (StringObject.isEmpty(from)) {
-            from = request.getHeader(HeaderKey.X_FROM_TERMINAL);
-        }
-        if (StringObject.isEmpty(from)) {
-            from = "browser";
-        }
-        return from;
+        return GlobalValueUtils.getFromTerminal(request);
     }
 
     public static String getFromIp(HttpServletRequest request) {
-        String ip = request.getHeader(HeaderKey.X_REAL_IP);
-        if (StringObject.isEmpty(ip) == false) {
-            return ip;
-        }
-        ip = request.getHeader(HeaderKey.X_FORWARDED_FOR);
-        if (StringObject.isEmpty(ip) == false) {
-            int at = ip.indexOf(",");
-            if (at < 0) {
-                return ip;
-            }
-            return ip.substring(0, at);
-        }
-
-        return request.getRemoteAddr();
-    }
-
-    public static String getFromUuid(HttpServletRequest request) {
-        String from = request.getParameter(HeaderKey.X_FROM_UUID);
-        if (StringObject.isEmpty(from)) {
-            from = request.getHeader(HeaderKey.X_FROM_UUID);
-        }
-        return from;
+        return GlobalValueUtils.getFromIp(request);
     }
 }
