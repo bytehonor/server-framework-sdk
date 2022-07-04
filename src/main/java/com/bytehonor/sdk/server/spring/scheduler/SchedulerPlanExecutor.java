1fbc2bddcb2634734bb7d6a0ddebf3ca20af8b7b
lijianqiang <newphper@126.com>
2022-07-04 10:59:29 +0800

    1.6.17

--- src/main/java/com/bytehonor/sdk/server/spring/scheduler/SchedulerPlanExecutor.java
+++ src/main/java/com/bytehonor/sdk/server/spring/scheduler/SchedulerPlanExecutor.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bytehonor.sdk.lang.spring.thread.SafeRunner;
 import com.bytehonor.sdk.server.spring.scheduler.plan.SchedulerPlan;
 
@@ -15,17 +18,24 @@ import com.bytehonor.sdk.server.spring.scheduler.plan.SchedulerPlan;
  */
 public class SchedulerPlanExecutor {
 
-    private static final ExecutorService SERVICE = Executors.newFixedThreadPool(1);
+    private static final Logger LOG = LoggerFactory.getLogger(SchedulerPlanExecutor.class);
+
+    private static final ExecutorService SERVICE = Executors.newFixedThreadPool(2);
 
     public static void run(final LocalDateTime ldt) {
         Objects.requireNonNull(ldt, "ldt");
 
+        int accepts = 0;
         List<SchedulerPlan> plans = SchedulerPlanFactory.plans();
         for (SchedulerPlan plan : plans) {
-            if (plan.accept(ldt)) {
-                doRun(plan.create(ldt));
+            if (plan.accept(ldt) == false) {
+                continue;
             }
+            doRun(plan.create(ldt));
+            accepts++;
         }
+
+        LOG.info("ldt:{}, accepts:{}, plans:{}", ldt, accepts, plans.size());
     }
 
     public static void doRun(SafeRunner runner) {
