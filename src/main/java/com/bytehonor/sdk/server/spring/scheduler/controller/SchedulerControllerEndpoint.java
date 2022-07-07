163b241a6442823e71a42319f20cf07c62fe6381
lijianqiang <newphper@126.com>
2022-07-07 16:06:34 +0800

    ControllerEndpoint

--- src/main/java/com/bytehonor/sdk/server/spring/scheduler/controller/SchedulerControllerEndpoint.java
+++ src/main/java/com/bytehonor/sdk/server/spring/scheduler/controller/SchedulerControllerEndpoint.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bytehonor.sdk.define.spring.result.IntegerResultVO;
 import com.bytehonor.sdk.server.spring.scheduler.stats.PlanStatsHandler;
 
-@ControllerEndpoint(id = "scheduler", enableByDefault = true)
+@ControllerEndpoint(id = "scheduler")
 public class SchedulerControllerEndpoint {
 
     @ResponseBody
-    @GetMapping("/scheduler/print")
+    @GetMapping("print")
     public IntegerResultVO schedulerPrint() {
         PlanStatsHandler.print();
         return IntegerResultVO.of(PlanStatsHandler.size());
