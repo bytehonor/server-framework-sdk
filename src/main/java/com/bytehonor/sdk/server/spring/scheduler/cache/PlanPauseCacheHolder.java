f0b6f31a358be4b3436ea568d6978b21d4d9cbcf
lijianqiang <343891954@qq.com>
2022-07-09 17:36:54 +0800

    PlanPauseCacheHolder

--- src/main/java/com/bytehonor/sdk/server/spring/scheduler/cache/PlanPauseCacheHolder.java
+++ src/main/java/com/bytehonor/sdk/server/spring/scheduler/cache/PlanPauseCacheHolder.java
@@ -1,49 +1,28 @@
 package com.bytehonor.sdk.server.spring.scheduler.cache;
 
+import java.util.HashSet;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
+import java.util.Set;
 
 public class PlanPauseCacheHolder {
 
-    private static int CAPACITY = 512;
-
-    private static Cache<String, Boolean> CACHE = CacheBuilder.newBuilder().initialCapacity(CAPACITY) // 设置初始容量为100
-            .maximumSize(10 * CAPACITY) // 设置缓存的最大容量
-            .expireAfterWrite(30, TimeUnit.DAYS) // 设置缓存在写入一分钟后失效
-            .concurrencyLevel(20) // 设置并发级别为10
-            .build(); // .recordStats() // 开启缓存统计
-
-    private static void put(String key, Boolean value) {
-        Objects.requireNonNull(key, "key");
-        Objects.requireNonNull(value, "value");
-
-        CACHE.put(key, value);
-    }
-
-    private static Boolean getIfPresent(String key) {
-        Objects.requireNonNull(key, "key");
-
-        return CACHE.getIfPresent(key);
-    }
-
-    private static void invalidate(String key) {
-        Objects.requireNonNull(key, "key");
-
-        CACHE.invalidate(key);
-    }
+    private static final Set<String> CACHE = new HashSet<String>(1024);
 
     public static void pause(String name) {
-        put(name, true);
+        Objects.requireNonNull(name, "name");
+
+        CACHE.add(name);
     }
 
     public static void play(String name) {
-        invalidate(name);
+        Objects.requireNonNull(name, "name");
+
+        CACHE.remove(name);
     }
 
     public static boolean isPause(String name) {
-        return getIfPresent(name) != null;
+        Objects.requireNonNull(name, "name");
+
+        return CACHE.contains(name);
     }
 }
