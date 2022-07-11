baeceb106ba178f25fcf684996e5e207e7c0e993
lijianqiang <343891954@qq.com>
2022-07-11 22:10:55 +0800

    1.7.24

--- src/main/java/com/bytehonor/sdk/server/spring/scheduler/plan/TimePlanStatus.java
+++ src/main/java/com/bytehonor/sdk/server/spring/scheduler/plan/TimePlanStatus.java
@@ -1,18 +1,27 @@
 package com.bytehonor.sdk.server.spring.scheduler.plan;
 
+import com.bytehonor.sdk.server.spring.context.ServerContext;
+
 /**
  * @author lijianqiang
  *
  */
 public class TimePlanStatus {
 
+    private final String server;
+
     private final String name;
 
     private final Boolean paused;
 
     public TimePlanStatus(String name, Boolean paused) {
+        this.server = ServerContext.self().getId();
         this.name = name;
         this.paused = paused;
+    }
+
+    public String getServer() {
+        return server;
     }
 
     public String getName() {
