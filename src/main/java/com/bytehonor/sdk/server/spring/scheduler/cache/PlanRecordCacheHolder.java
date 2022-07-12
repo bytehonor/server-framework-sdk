15293c6693b3892b86d3132958660842e6e85495
lijianqiang <newphper@126.com>
2022-07-12 09:16:40 +0800

    1.7.24

--- src/main/java/com/bytehonor/sdk/server/spring/scheduler/cache/PlanRecordCacheHolder.java
+++ src/main/java/com/bytehonor/sdk/server/spring/scheduler/cache/PlanRecordCacheHolder.java
@@ -9,11 +9,11 @@ import com.google.common.cache.CacheBuilder;
 
 public class PlanRecordCacheHolder {
 
-    private static int CAPACITY = 60 * 24 * 512;
+    private static int CAPACITY = 1024;
 
     private static Cache<String, Long> CACHE = CacheBuilder.newBuilder().initialCapacity(CAPACITY) // 设置初始容量为100
             .maximumSize(128 * CAPACITY) // 设置缓存的最大容量
-            .expireAfterWrite(1, TimeUnit.DAYS) // 设置缓存在写入一分钟后失效
+            .expireAfterWrite(15, TimeUnit.DAYS) // 设置缓存在写入一分钟后失效
             .concurrencyLevel(20) // 设置并发级别为10
             .build(); // .recordStats() // 开启缓存统计
 
