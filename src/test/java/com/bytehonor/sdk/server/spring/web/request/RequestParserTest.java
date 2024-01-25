54c0859d0280554ffc2f9cee4135837a8a24af57
lijianqiang <newphper@126.com>
2024-01-25 17:44:53 +0800

    test

--- src/test/java/com/bytehonor/sdk/server/spring/web/request/RequestParserTest.java
+++ src/test/java/com/bytehonor/sdk/server/spring/web/request/RequestParserTest.java
@@ -11,14 +11,25 @@ import com.bytehonor.sdk.lang.spring.query.QueryCondition;
 public class RequestParserTest {
 
     @Test
-    public void test() {
+    public void test1() {
 
         KeyValueMap map = new KeyValueMap();
         map.put("create_at", 123L);
 
         QueryCondition condition = RequestParser.condition(Student.class, QueryLogic.AND, map);
         
-        assertTrue("*test*", condition.has(Student::getCreateAt));
+        assertTrue("*test1*", condition.has(Student::getCreateAt));
+    }
+    
+    @Test
+    public void test2() {
+
+        KeyValueMap map = new KeyValueMap();
+        map.put("createAt", 123L);
+
+        QueryCondition condition = RequestParser.condition(Student.class, QueryLogic.AND, map);
+        
+        assertTrue("*test2*", condition.has(Student::getCreateAt));
     }
 
 }
