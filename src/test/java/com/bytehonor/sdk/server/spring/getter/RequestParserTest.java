6fc9ecee90319a9c91e4e13ddbc4acbbff754422
lijianqiang <newphper@126.com>
2024-01-25 17:29:11 +0800

    test

--- src/test/java/com/bytehonor/sdk/server/spring/getter/RequestParserTest.java
+++ src/test/java/com/bytehonor/sdk/server/spring/getter/RequestParserTest.java
@@ -1,14 +1,24 @@
 package com.bytehonor.sdk.server.spring.getter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.bytehonor.sdk.lang.spring.constant.QueryLogic;
+import com.bytehonor.sdk.lang.spring.core.KeyValueMap;
+import com.bytehonor.sdk.lang.spring.query.QueryCondition;
 
 public class RequestParserTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
+
+        KeyValueMap map = new KeyValueMap();
+        map.put("create_at", 123L);
+
+        QueryCondition condition = RequestParser.condition(Student.class, QueryLogic.AND, map);
+        
+        assertTrue("*test*", condition.has(Student::getCreateAt));
     }
 
 }
