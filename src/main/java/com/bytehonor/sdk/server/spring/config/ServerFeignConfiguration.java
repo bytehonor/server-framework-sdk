eb2d420eba84496df0aeb78d214b513a62785a25
lijianqiang <newphper@126.com>
2024-05-16 11:46:10 +0800

    2.0.12

--- src/main/java/com/bytehonor/sdk/server/spring/config/ServerFeignConfiguration.java
+++ src/main/java/com/bytehonor/sdk/server/spring/config/ServerFeignConfiguration.java
@@ -3,21 +3,17 @@ package com.bytehonor.sdk.server.spring.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.bytehonor.sdk.server.spring.web.feign.FeignRequestInterceptor;
 
-import feign.Feign;
-
+//@ConditionalOnClass(Feign.class)
 @Configuration
 @ConditionalOnWebApplication
-@ConditionalOnClass(Feign.class)
-@AutoConfigureAfter(FeignAutoConfiguration.class)
+@AutoConfigureAfter(ServerWebConfiguration.class)
 public class ServerFeignConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(ServerFeignConfiguration.class);
