b294427b82c245615ef8b7c8001f4e5b28c251e4
lijianqiang <newphper@126.com>
2024-05-16 11:47:50 +0800

    clear

--- src/main/java/com/bytehonor/sdk/server/spring/config/ServerFeignConfiguration.java
+++ src/main/java/com/bytehonor/sdk/server/spring/config/ServerFeignConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 
 import com.bytehonor.sdk.server.spring.web.feign.FeignRequestInterceptor;
 
-//@ConditionalOnClass(Feign.class)
 @Configuration
 @ConditionalOnWebApplication
 @AutoConfigureAfter(ServerWebConfiguration.class)
