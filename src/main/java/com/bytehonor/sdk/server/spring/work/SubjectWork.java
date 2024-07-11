6fb3c21f3cbbfaecc1efc7c997015087d01a1f63
lijianqiang <newphper@126.com>
2024-07-11 14:21:32 +0800

    clear

--- src/main/java/com/bytehonor/sdk/server/spring/work/SubjectWork.java
+++ src/main/java/com/bytehonor/sdk/server/spring/work/SubjectWork.java
@@ -2,7 +2,15 @@ package com.bytehonor.sdk.server.spring.work;
 
 public interface SubjectWork {
 
+    /**
+     * 主题
+     * 
+     * @return
+     */
     public String subject();
 
+    /**
+     * 启动，只启动一次
+     */
     public void start();
 }
